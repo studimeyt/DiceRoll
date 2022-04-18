4e0533d79d1a8245fea493fe7a47379895bf6658
Harshit Srivastava <harshit.srivastava018@gmail.com>
2022-04-18 19:46:10 +0530

    Final Commit

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+/**
+ * This activity allows the user to roll a dice and view the result
+ * on the screen.
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button= findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice()
+            val toast = Toast.makeText(this,"You shall know PAINx2!",Toast.LENGTH_SHORT)
+            toast.show()
+
+        }
+
+
+    }
+    /**
+     * Roll the dice and update the screen with the result.
+     */
+    private fun rollDice() {
+        // Create new Dice object with 6 sides and roll it
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceRoll4 = dice.roll()
+
+        // Update the screen with the dice roll
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRoll.toString()
+        val resultTextView4: TextView = findViewById(R.id.textView4)
+        resultTextView4.text = diceRoll4.toString()
+
+    }
+}
+
+class Dice(private val numSides:Int){
+    fun roll():Int{
+        return(1..numSides).random()
+    }
+}
